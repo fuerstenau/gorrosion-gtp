29e1d5730cb34bf1ed2631b8b93f962e7b248cd8
Peter Fürstenau <fuerstenau@mathematik.tu-darmstadt.de>
2018-12-10 03:26:40 +0100

    Comment on usage of the Byte alias

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,13 @@
 #[macro_use]
 extern crate nom;
 
+/// Despite its name,
+/// GTP feels like a binary protocol
+/// and we will treat it as such.
+/// Consequently, all “text” is a sequence of bytes.
+/// To emphasize that these bytes are not used for their numerical value
+/// but rather their property of “most generic kind of data”
+/// we will explicitly refer to them as `Byte` instead of `u8`.
 type Byte = u8;
 
 mod data;
